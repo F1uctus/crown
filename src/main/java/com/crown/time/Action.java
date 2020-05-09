9daf565ddea47d7aaeec577b203ffc1c758c901a
Nikitin Ilya <f1uctus@mail.ru>
2020-05-10 02:40:44 +0300

    :sparkles: Add `Action.changeProperty` helper method

--- src/main/java/com/crown/time/Action.java
+++ src/main/java/com/crown/time/Action.java
@@ -2,8 +2,24 @@ package com.crown.time;
 
 import com.crown.i18n.ITemplate;
 
+import java.util.function.Function;
+
 public abstract class Action {
     public abstract ITemplate perform();
 
     public abstract ITemplate rollback();
+
+    public Action changeProperty(Function<Integer, ITemplate> changer, int delta) {
+        return new Action() {
+            @Override
+            public ITemplate perform() {
+                return changer.apply(delta);
+            }
+
+            @Override
+            public ITemplate rollback() {
+                return changer.apply(-delta);
+            }
+        };
+    }
 }
