e57a3502888396bbc117023b0f1bd01fb7b9da80
Nikitin Ilya <f1uctus@mail.ru>
2020-05-13 03:23:21 +0300

    :alembic: Experimenting with Kryo

--- src/main/java/com/crown/common/Cloner.java
+++ src/main/java/com/crown/common/Cloner.java
@@ -1,13 +1,22 @@
 package com.crown.common;
 
+import com.crown.BaseGameState;
+import com.crown.time.Action;
 import com.crown.time.Timeline;
+import com.crown.time.VirtualClock;
 import com.esotericsoftware.kryo.kryo5.Kryo;
+
+import java.util.ArrayList;
 
 public class Cloner {
     public final Kryo kryo = new Kryo();
     public static final Cloner instance = new Cloner();
 
     private Cloner() {
+        kryo.register(ArrayList.class);
         kryo.register(Timeline.class);
+        kryo.register(VirtualClock.class);
+        kryo.register(BaseGameState.class);
+        kryo.register(Action.class);
     }
 }
