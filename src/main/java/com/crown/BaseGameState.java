7c1226c92f9bccb865045bc5349c32eff7a038d1
Nikitin Ilya <f1uctus@mail.ru>
2020-06-21 00:16:30 +0300

    :link: Remove `Map` reference from `BaseGameState`

--- src/main/java/com/crown/BaseGameState.java
+++ src/main/java/com/crown/BaseGameState.java
@@ -2,17 +2,14 @@ package com.crown;
 
 import com.crown.common.ObjectCollection;
 import com.crown.creatures.Creature;
-import com.crown.maps.Map;
 import com.crown.time.Timeline;
 
 import java.io.Serializable;
 
 public class BaseGameState implements Serializable {
     public final ObjectCollection<Creature> players = new ObjectCollection<>();
-    public final Map map;
 
-    public BaseGameState(Map map) {
-        this.map = map;
+    public BaseGameState() {
     }
 
     protected void addNewPlayer(Creature p) {
