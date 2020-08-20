bdea7a80e982e82f967ba20a09c955c62943f058
Nikitin Ilya <f1uctus@mail.ru>
2020-08-21 01:54:39 +0300

    New free random map points API

--- src/main/java/com/crown/common/utils/Random.java
+++ src/main/java/com/crown/common/utils/Random.java
@@ -49,63 +49,39 @@ public class Random {
     }
 
     /**
-     * Returns any point of map not occupied by obstacle.
-     * If failed attempts count reaches map volume, null is returned.
+     * Returns a random point of map not occupied by obstacle.
      */
-    public static Point3D getFreePoint(Map m) {
-        return getFreePoint(m, new Point3D(-1, -1, -1));
-    }
-
-    /**
-     * Returns any point of map not occupied by obstacle.
-     * If failed attempts count reaches map volume, null is returned.
-     *
-     * @param fixedPt Specifies coordinates that should be fixed, not random.
-     *                -1 for each coordinate means that it's random.
-     *                (e.g to get point with z = 1 exactly,
-     *                invoke it with Point3D(-1, -1, 1)).
-     */
-    public static Point3D getFreePoint(Map m, Point3D fixedPt) {
-        return getFreePoint(
-            m,
-            fixedPt,
-            m.xSize * m.ySize * m.zSize
-        );
-    }
-
-    /**
-     * Returns any point of map not occupied by obstacle.
-     * If failed attempts count reaches max value, null is returned.
-     *
-     * @param fixedPt Specifies coordinates that should be fixed, not random.
-     *                -1 for each coordinate means that it's random.
-     *                (e.g to get point with z = 1 exactly,
-     *                invoke it with Point3D(-1, -1, 1)).
-     */
-    static Point3D getFreePoint(
-        Map m,
-        Point3D fixedPt,
-        long maxAttempts
-    ) {
+    public static Point3D getFreePoint(Map map) {
         Point3D pt;
         MapObject obj;
         long attempts = -1;
         do {
-            if (++attempts == maxAttempts) {
+            if (++attempts == map.xSize * map.ySize * map.zSize) {
                 return null;
             }
 
-            pt = Random.getPoint(m);
-            if (fixedPt.x != -1) {
-                pt.x = fixedPt.x;
+            pt = Random.getPoint(map);
+            obj = map.get(pt);
+        } while (obj != null && obj.getMapWeight() == MapWeight.OBSTACLE);
+
+        return pt;
+    }
+
+    /**
+     * Returns a random point of map not occupied by obstacle
+     * for object with specified size.
+     */
+    public static Point3D getFreePoint(Map map, int xSize, int ySize) {
+        Point3D pt;
+        MapObject obj;
+        long attempts = -1;
+        do {
+            if (++attempts == map.xSize * map.ySize * map.zSize) {
+                return null;
             }
-            if (fixedPt.y != -1) {
-                pt.y = fixedPt.y;
-            }
-            if (fixedPt.z != -1) {
-                pt.z = fixedPt.z;
-            }
-            obj = m.get(pt);
+
+            pt = Random.getPoint(map, xSize, ySize);
+            obj = map.get(pt);
         } while (obj != null && obj.getMapWeight() == MapWeight.OBSTACLE);
 
         return pt;
