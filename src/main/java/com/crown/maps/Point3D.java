15ef902fb6bb4e3d14fa1b7850d34a13fc1491f6
Nikitin Ilya <f1uctus@mail.ru>
2020-08-21 04:59:24 +0300

    Point3D API enhancement
    - Add discrete & unary `plus/minus`
    - `min/max` directions
    - `insideOf(a, b)` check
    - Make regular `min/max` static

--- src/main/java/com/crown/maps/Point3D.java
+++ src/main/java/com/crown/maps/Point3D.java
@@ -28,6 +28,8 @@ public class Point3D implements Serializable {
         this.z = z;
     }
 
+    public static final Point3D ZERO = new Point3D(0, 0, 0);
+
     /**
      * Copy constructor for a point.
      */
@@ -68,7 +70,7 @@ public class Point3D implements Serializable {
     }
 
     /**
-     * Returns Point3D that is the sum of this and
+     * Returns new point that is the sum of this and
      * another points. The sum of two points is another point
      * whose x, y, and z values are the sum of
      * the two point x, y, and z values.
@@ -82,7 +84,7 @@ public class Point3D implements Serializable {
     }
 
     /**
-     * Returns Point3D representing difference of
+     * Returns new point representing difference of
      * this point and another point. The difference of two points is
      * another point whose x, y, and z values are
      * the difference of the two point x, y, and z values.
@@ -93,6 +95,58 @@ public class Point3D implements Serializable {
             y - point.y,
             z - point.z
         );
+    }
+
+    /**
+     * Returns new point with all coordinates increased by deltas.
+     */
+    public Point3D plus(int dx, int dy, int dz) {
+        return new Point3D(
+            x + dx,
+            y + dy,
+            z + dz
+        );
+    }
+
+    /**
+     * Returns new point with all coordinates decreased by deltas.
+     */
+    public Point3D minus(int dx, int dy, int dz) {
+        return new Point3D(
+            x - dx,
+            y - dy,
+            z - dz
+        );
+    }
+
+    /**
+     * Returns new point with all coordinates increased by value.
+     */
+    public Point3D plus(int value) {
+        return new Point3D(
+            x + value,
+            y + value,
+            z + value
+        );
+    }
+
+    /**
+     * Returns new point with all coordinates decreased by value.
+     */
+    public Point3D minus(int value) {
+        return new Point3D(
+            x - value,
+            y - value,
+            z - value
+        );
+    }
+
+    /**
+     * Returns new point representing a point with
+     * the negative of this point's coordinates
+     */
+    public Point3D minus() {
+        return new Point3D(-x, -y, -z);
     }
 
     /**
@@ -110,32 +164,50 @@ public class Point3D implements Serializable {
      * Returns minimal point, composed from 2 provided points.
      * (Math.min for each coordinate).
      */
-    public Point3D min(Point3D other) {
+    public static Point3D min(Point3D a, Point3D b) {
         return new Point3D(
-            Math.min(x, other.x),
-            Math.min(y, other.y),
-            Math.min(z, other.z)
+            Math.min(a.x, b.x),
+            Math.min(a.y, b.y),
+            Math.min(a.z, b.z)
         );
+    }
+
+    /**
+     * Returns min from coordinates of this point.
+     */
+    public int minDirection() {
+        return Math.min(Math.min(x, y), z);
     }
 
     /**
      * Returns maximal point, composed from 2 provided points.
      * (Math.max for each coordinate).
      */
-    public Point3D max(Point3D other) {
+    public static Point3D max(Point3D a, Point3D b) {
         return new Point3D(
-            Math.max(x, other.x),
-            Math.max(y, other.y),
-            Math.max(z, other.z)
+            Math.max(a.x, b.x),
+            Math.max(a.y, b.y),
+            Math.max(a.z, b.z)
         );
     }
 
     /**
-     * Returns Point3D representing a point with
-     * the negative of this point's coordinates
+     * Returns max from coordinates of this point.
      */
-    public Point3D minus() {
-        return new Point3D(-x, -y, -z);
+    public int maxDirection() {
+        return Math.max(Math.max(x, y), z);
+    }
+
+    /**
+     * Checks if this point is inside of a parallelogram
+     * defined by given a & b points.
+     * In other words, checks if each coordinate of this point
+     * is between the given points (e.g min <= x <= max, same for y and z).
+     */
+    public boolean isInsideOf(Point3D a, Point3D b) {
+        return x >= a.x && x <= b.x
+            && y >= a.y && y <= b.y
+            && z >= a.z && z <= b.z;
     }
 
     @Override
