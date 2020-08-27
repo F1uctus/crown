6b2fad7f5ea5777aacf88cde7a8cdb2777cbac62
Nikitin Ilya <f1uctus@mail.ru>
2020-08-27 17:22:32 +0300

    Create `IMap` interface

--- src/main/java/com/crown/maps/IMap.java
+++ src/main/java/com/crown/maps/IMap.java
@@ -1,2 +1,15 @@
-package com.crown.maps;public interface IMap {
+package com.crown.maps;
+
+import org.jetbrains.annotations.NotNull;
+
+public interface IMap {
+    boolean inBounds(int x, int y);
+
+    boolean inBounds(int x, int y, int z);
+
+    boolean inBounds(Point3D point);
+
+    boolean isObstacle(@NotNull Point3D point);
+
+    boolean isTransparent(@NotNull Point3D point);
 }
