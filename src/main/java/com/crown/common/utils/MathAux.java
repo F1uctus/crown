8da45afd5ae5b5563246b3d5dd6b35df052856a7
Nikitin Ilya <f1uctus@mail.ru>
2020-08-27 17:21:59 +0300

    More functions for `MathAux`

--- src/main/java/com/crown/common/utils/MathAux.java
+++ src/main/java/com/crown/common/utils/MathAux.java
@@ -21,4 +21,57 @@ public class MathAux {
     public static int clampDown(int value, int min, int max) {
         return value >= min && value <= max ? value : min;
     }
+
+    /**
+     * Returns 1 if value >= 0, and returns 0 otherwise.
+     */
+    public static int positiveSign(int value) {
+        return value >= 0 ? 1 : 0;
+    }
+
+    /**
+     * @return the value {@code 0} if {@code x == 0};
+     * a value {@code -1} if {@code x < 0}; and
+     * a value {@code 1} if {@code x > 0}
+     */
+    public static int signum(int value) {
+        return Integer.compare(value, 0);
+    }
+
+    /**
+     * Variadic version of Math.max
+     */
+    public static int max(int a, int b, int... c) {
+        int max = Math.max(a, b);
+        if (c != null) {
+            for (int i : c) {
+                if (i > max) max = i;
+            }
+        }
+        return max;
+    }
+
+    /**
+     * Variadic version of Math.min
+     */
+    public static int min(int a, int b, int... c) {
+        int min = Math.min(a, b);
+        if (c != null) {
+            for (int i : c) {
+                if (i < min) min = i;
+            }
+        }
+        return min;
+    }
+
+    public static int normalizeAngle(int angle) {
+        if (angle < 0) {
+            angle %= 360;
+            angle += 360;
+        }
+        if (angle > 360) {
+            angle %= 360;
+        }
+        return angle;
+    }
 }
---
+++ src/main/java/com/crown/maps/IMap.java
@@ -0,0 +1,2 @@
+package com.crown.maps;public interface IMap {
+}
