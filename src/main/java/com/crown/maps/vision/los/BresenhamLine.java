759ac97a58222d308af35a012b034951cf69f79b
Nikitin Ilya <f1uctus@mail.ru>
2020-08-29 23:57:19 +0300

    :beetle: [0.8.0i] Alternative`BresenhamLine` implementation

--- src/main/java/com/crown/maps/vision/los/BresenhamLine.java
+++ src/main/java/com/crown/maps/vision/los/BresenhamLine.java
@@ -9,87 +9,71 @@ import java.util.ArrayList;
  */
 public final class BresenhamLine {
     public static Point3D[] getFor(Point3D start, Point3D end) {
-        int x = start.x;
-        int y = start.y;
-        int z = start.z;
+        int pointX = start.x;
+        int pointY = start.y;
+        int pointZ = start.z;
+        final Point3D d = end.minus(start);
+        final Point3D inc = d.positiveSign();
+        final Point3D abs = d.abs();
+        int dx2 = abs.x << 1;
+        int dy2 = abs.y << 1;
+        int dz2 = abs.z << 1;
 
         var points = new ArrayList<Point3D>();
-        points.add(start);
 
-        final Point3D d = end.minus(start);
-        final Point3D dSign = d.positiveSign();
-        final Point3D dAbs = d.abs();
-
-        final int px, py, pz, d0, d1, d2, jx1, jy1, jz1, jx2, jy2, jz2;
-        if (dAbs.x > dAbs.y && dAbs.x > dAbs.z) {
-            px = dSign.x;
-            py = 0;
-            pz = 0;
-            jx1 = 0;
-            jy1 = dSign.y;
-            jz1 = 0;
-            jx2 = 0;
-            jy2 = 0;
-            jz2 = dSign.z;
-            d0 = dAbs.x;
-            d1 = dAbs.y;
-            d2 = dAbs.z;
-        } else if (dAbs.y > dAbs.x && dAbs.y > dAbs.z) {
-            px = 0;
-            py = dSign.y;
-            pz = 0;
-            jx1 = dSign.x;
-            jy1 = 0;
-            jz1 = 0;
-            jx2 = 0;
-            jy2 = 0;
-            jz2 = dSign.z;
-            d0 = dAbs.y;
-            d1 = dAbs.x;
-            d2 = dAbs.z;
+        if (abs.x >= abs.y && abs.x >= abs.z) {
+            int err1 = dy2 - abs.x;
+            int err2 = dz2 - abs.x;
+            for (int i = 0; i < abs.x; i++) {
+                points.add(new Point3D(pointX, pointY, pointZ));
+                if (err1 > 0) {
+                    pointY += inc.y;
+                    err1 -= dx2;
+                }
+                if (err2 > 0) {
+                    pointZ += inc.z;
+                    err2 -= dx2;
+                }
+                err1 += dy2;
+                err2 += dz2;
+                pointX += inc.x;
+            }
+        } else if (abs.y >= abs.x && abs.y >= abs.z) {
+            int err1 = dx2 - abs.y;
+            int err2 = dz2 - abs.y;
+            for (int i = 0; i < abs.y; i++) {
+                points.add(new Point3D(pointX, pointY, pointZ));
+                if (err1 > 0) {
+                    pointX += inc.x;
+                    err1 -= dy2;
+                }
+                if (err2 > 0) {
+                    pointZ += inc.z;
+                    err2 -= dy2;
+                }
+                err1 += dx2;
+                err2 += dz2;
+                pointY += inc.y;
+            }
         } else {
-            px = 0;
-            py = 0;
-            pz = dSign.z;
-            jx1 = dSign.x;
-            jy1 = 0;
-            jz1 = 0;
-            jx2 = 0;
-            jy2 = dSign.y;
-            jz2 = 0;
-            d0 = dAbs.z;
-            d1 = dAbs.x;
-            d2 = dAbs.y;
-        }
-
-        int delta1 = d0 / 2;
-        int delta2 = delta1;
-
-        for (int p = 0; p < d0; p++) {
-            delta1 -= d1;
-            delta2 -= d2;
-
-            if (delta1 < 0) {
-                delta1 += d0;
-                x += jx1;
-                y += jy1;
-                z += jz1;
+            int err1 = dy2 - abs.z;
+            int err2 = dx2 - abs.z;
+            for (int i = 0; i < abs.z; i++) {
+                points.add(new Point3D(pointX, pointY, pointZ));
+                if (err1 > 0) {
+                    pointY += inc.y;
+                    err1 -= dz2;
+                }
+                if (err2 > 0) {
+                    pointX += inc.x;
+                    err2 -= dz2;
+                }
+                err1 += dy2;
+                err2 += dx2;
+                pointZ += inc.z;
             }
-
-            if (delta2 < 0) {
-                delta2 += d0;
-                x += jx2;
-                y += jy2;
-                z += jz2;
-            }
-
-            x += px;
-            y += py;
-            z += pz;
-
-            points.add(new Point3D(x, y, z));
         }
-
+        points.add(new Point3D(pointX, pointY, pointZ));
         return points.toArray(new Point3D[0]);
     }
 }
