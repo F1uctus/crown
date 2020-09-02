e6e05fb7143c718289d44bd79bf385bf9c7d26cf
Nikitin Ilya <f1uctus@mail.ru>
2020-09-02 11:16:39 +0300

    :white_check_mark: Add tests for `Direction`

---
+++ src/test/java/DirectionTest.java
@@ -0,0 +1,72 @@
+import com.crown.maps.Direction;
+import com.crown.maps.Point3D;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DirectionTest {
+    @Test
+    public void testFromPoint() {
+        assertEquals(Direction.fromPoint(Point3D.ZERO), Direction.none);
+
+        assertEquals(Direction.fromPoint(Point3D.ZERO.withZ(1)), Direction.up);
+
+        assertEquals(Direction.fromPoint(Point3D.ZERO.withZ(-1)), Direction.down);
+
+        // north
+        var point = new Point3D(0, -10, 0);
+        assertEquals(Direction.fromPoint(point), Direction.n);
+
+        point = new Point3D(0, -1, 0);
+        assertEquals(Direction.fromPoint(point), Direction.n);
+
+        // north-east
+        point = new Point3D(10, -10, 0);
+        assertEquals(Direction.fromPoint(point), Direction.ne);
+
+        point = new Point3D(1, -1, 0);
+        assertEquals(Direction.fromPoint(point), Direction.ne);
+
+        // north-west
+        point = new Point3D(-10, -10, 0);
+        assertEquals(Direction.fromPoint(point), Direction.nw);
+
+        point = new Point3D(-1, -1, 0);
+        assertEquals(Direction.fromPoint(point), Direction.nw);
+
+        // south
+        point = new Point3D(0, 10, 0);
+        assertEquals(Direction.fromPoint(point), Direction.s);
+
+        point = new Point3D(0, 1, 0);
+        assertEquals(Direction.fromPoint(point), Direction.s);
+
+        // south-east
+        point = new Point3D(10, 10, 0);
+        assertEquals(Direction.fromPoint(point), Direction.se);
+
+        point = new Point3D(1, 1, 0);
+        assertEquals(Direction.fromPoint(point), Direction.se);
+
+        // south-west
+        point = new Point3D(-10, 10, 0);
+        assertEquals(Direction.fromPoint(point), Direction.sw);
+
+        point = new Point3D(-1, 1, 0);
+        assertEquals(Direction.fromPoint(point), Direction.sw);
+
+        // east
+        point = new Point3D(10, 0, 0);
+        assertEquals(Direction.fromPoint(point), Direction.e);
+
+        point = new Point3D(1, 0, 0);
+        assertEquals(Direction.fromPoint(point), Direction.e);
+
+        // west
+        point = new Point3D(-10, 0, 0);
+        assertEquals(Direction.fromPoint(point), Direction.w);
+
+        point = new Point3D(-1, 0, 0);
+        assertEquals(Direction.fromPoint(point), Direction.w);
+    }
+}
--- src/test/java/NamedObjectTest.java
+++ src/test/java/NamedObjectTest.java
@@ -6,13 +6,13 @@ import com.crown.common.NamedObject;
 public class NamedObjectTest {
     @Test
     public void testGetKeyName() {
-        NamedObject no = new NamedObject("  Some Object  ") { };
+        var no = new NamedObject("  Some Object  ") { };
         assertEquals(no.getKeyName(), "some object");
     }
 
     @Test
-    public void testKeyName() {
-        NamedObject no = new NamedObject("  Some Object  ") { };
+    public void testSetKeyName() {
+        var no = new NamedObject("  Some Object  ") { };
         no.setKeyName("  Other Object  ");
         assertEquals(no.getKeyName(), "other object");
     }
